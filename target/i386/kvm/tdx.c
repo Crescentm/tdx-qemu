intel/qemu-td-partitioning

i386/tdx: Update tdx_cpuid_lookup[]
Add several fixed0/fixed1 type CPUIDs to TDX CPUID lookup table according
to the CPUID Virtualization in TDX Module v1.5 ABI Specification.

Regarding the added CPUIDs, the difference between the public spec of
TDX 1.0 and 1.5 is shown as follows:
                                          TDX 1.0               TDX 1.5
 **CPUID.0x01:ECX.DTES64[2]               native                fixed1**
 **CPUID.0x01:ECX.DSCPL[4]                native                fixed1**
   CPUID.0x07:EDX.SGX-KEYS[1]             fixed0                fixed0
 **CPUID.0x07:EDX.L1D_FLUSH[28]           native                fixed1**
   CPUID.0x12.0x0:EAX[31:0]               fixed0                fixed0
   CPUID.0x12.0x0:EBX[31:0]               fixed0                fixed0
   CPUID.0x12.0x1:EAX[31:0]               fixed0                fixed0

Although the virtualization of three CPUIDs is not the same in TDX1.0
and 1.5, they won’t break the compatibility, since
update_tdx_cpuid_lookup_by_tdx_caps() uses TDSYSINFO_STRUCT.cpuid to
drop unsupported bits.

Signed-off-by: Qian Wen <qian.wen@intel.com>

target/i386/kvm/tdx.c
@@ -242,6 +242,7 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
         .tdx_fixed0 =
             CPUID_EXT_VMX | CPUID_EXT_SMX | BIT(16) /* Reserved */,
         .tdx_fixed1 =
+            CPUID_EXT_DTES64 | CPUID_EXT_DSCPL |
             CPUID_EXT_CX16 | CPUID_EXT_PDCM | CPUID_EXT_X2APIC |
             CPUID_EXT_AES | CPUID_EXT_XSAVE | CPUID_EXT_RDRAND |
             CPUID_EXT_HYPERVISOR,
@@ -274,8 +275,10 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
             CPUID_7_0_ECX_TME,
     },
     [FEAT_7_0_EDX] = {
+        .tdx_fixed0 = CPUID_7_0_EDX_SGX_KEYS,
         .tdx_fixed1 =
-            CPUID_7_0_EDX_SPEC_CTRL | CPUID_7_0_EDX_ARCH_CAPABILITIES |
+            CPUID_7_0_EDX_SPEC_CTRL |
+            CPUID_7_0_EDX_FLUSH_L1D | CPUID_7_0_EDX_ARCH_CAPABILITIES |
             CPUID_7_0_EDX_CORE_CAPABILITY | CPUID_7_0_EDX_SPEC_CTRL_SSBD,
         .vmm_fixup =
             CPUID_7_0_EDX_PCONFIG,
@@ -302,6 +305,15 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
     [FEAT_KVM] = {
         .inducing_ve = true,
         .supported_on_ve = TDX_SUPPORTED_KVM_FEATURES,
+    },
+    [FEAT_SGX_12_0_EAX] = {
+        .tdx_fixed0 = -1U,
+    },
+    [FEAT_SGX_12_0_EBX] = {
+        .tdx_fixed0 = -1U,
+    },
+    [FEAT_SGX_12_1_EAX] = {
+        .tdx_fixed0 = -1U,
     },
 };
 

Crescentm/tdx-qemu

target/i386/kvm/tdx.c
@@ -211,6 +211,7 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
             CPUID_EXT_VMX | CPUID_EXT_SMX |
             BIT(16),
         .tdx_fixed1 =
+            CPUID_EXT_DTES64 | CPUID_EXT_DSCPL |
             CPUID_EXT_CX16 | CPUID_EXT_PDCM | CPUID_EXT_X2APIC |
             CPUID_EXT_AES | CPUID_EXT_XSAVE | CPUID_EXT_RDRAND |
             CPUID_EXT_HYPERVISOR | CPUID_EXT_MONITOR,
@@ -243,8 +244,10 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
             CPUID_7_0_ECX_TME,
     },
     [FEAT_7_0_EDX] = {
+        .tdx_fixed0 = CPUID_7_0_EDX_SGX_KEYS,
         .tdx_fixed1 =
-            CPUID_7_0_EDX_SPEC_CTRL | CPUID_7_0_EDX_ARCH_CAPABILITIES |
+            CPUID_7_0_EDX_SPEC_CTRL |
+            CPUID_7_0_EDX_L1D_FLUSH | CPUID_7_0_EDX_ARCH_CAPABILITIES |
             CPUID_7_0_EDX_CORE_CAPABILITY | CPUID_7_0_EDX_SPEC_CTRL_SSBD,
         .vmm_fixup =
             CPUID_7_0_EDX_PCONFIG,
@@ -271,6 +274,15 @@ static KvmTdxCpuidLookup tdx_cpuid_lookup[FEATURE_WORDS] = {
     [FEAT_KVM] = {
         .inducing_ve = true,
         .supported_on_ve = TDX_SUPPORTED_KVM_FEATURES,
+    },
+    [FEAT_SGX_12_0_EAX] = {
+        .tdx_fixed0 = -1U,
+    },
+    [FEAT_SGX_12_0_EBX] = {
+        .tdx_fixed0 = -1U,
+    },
+    [FEAT_SGX_12_1_EAX] = {
+        .tdx_fixed0 = -1U,
     },
 };
 